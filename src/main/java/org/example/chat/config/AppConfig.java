3e6c272166dff482fb6fbd7db4651c95de64a7a7
Ivan <yougame2021hell@gmail.com>
2025-04-27 14:37:46 +0300

    final 1

--- src/main/java/org/example/chat/config/AppConfig.java
+++ src/main/java/org/example/chat/config/AppConfig.java
@@ -23,22 +23,6 @@ public class AppConfig extends GlobalMethodSecurityConfiguration {
         return new CommandLineRunner() {
             @Override
             public void run(String... strings) throws Exception {
-                userService.addUser("qwe",new BCryptPasswordEncoder().encode("qwe"));
-                userService.addUser("qw",new BCryptPasswordEncoder().encode("qw"));
-                List<String> users = new ArrayList<>();
-                users.add("qwe");
-                users.add("qw");
-                chatService.addChat("qwe123",false,users,"qwe");
-                messageService.addMessage(3L,"hello","qwe");
-                messageService.addMessage(3L,"hello","qw");
-                messageService.addMessage(3L,"hello","qw");
-                for (int i = 0; i < 100; i++){
-                    messageService.addMessage(3L,"hello_"+i,"qwe");
-                }
-                for (int i = 0; i < 100; i++){
-                    chatService.addChat("chat_"+i,true,users,"qwe");
-                }
-                messageService.hideMessageForUserById(6L,"qwe");
             }
         };
     }
